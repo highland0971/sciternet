1c193ef7bdef0c1be1adf2236dcd8d5d8bb7f3f7
highland0971 <joy.highland@gmail.com>
2016-03-26 10:01:38 +0800

    Added default user Login data model

---
+++ www_server/app/model/userLogin.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ * Created by highland0971 on 2016/3/26.
+ */
+public class userLogin {
+    private String email;
+    private String password;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
